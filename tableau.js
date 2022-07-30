b2ede03d685c29660152d33094cd6ae90de42b78
khayatjr <Khayatove@yahoo.com>
2022-07-31 00:55:47 +0200

    done 3

--- tableau.js
+++ tableau.js
@@ -1,6 +1,6 @@
 function initViz() {
     var containerDiv = document.getElementById("vizContainer"),
-    url = "https://public.tableau.com/views/dashboard1_omid/AlcoholandinfectionstatusandASTALT?:language=en-US&:display_count=n&:origin=viz_share_link";
+    url = "https://public.tableau.com/views/dashboard1_omid/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link";
 
     var viz = new tableau.Viz(containerDiv, url);
 }
