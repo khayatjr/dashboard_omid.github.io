70cf23c1646b6c6bb2f03a4c58c011fe1d7d373f
khayatjr <36375417+khayatjr@users.noreply.github.com>
2022-07-31 01:05:07 +0200

    Delete tableau.js

--- tableau.js
+++
@@ -1,6 +0,0 @@
-function initViz() {
-    var containerDiv = document.getElementById("vizContainer"),
-    url = "https://public.tableau.com/views/dashboard1_omid/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link";
-
-    var viz = new tableau.Viz(containerDiv, url);
-}
